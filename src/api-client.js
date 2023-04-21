907505d9b399be43195e992d90980db921a9e87d
Ildar Nazipov <ildar.nazipov@gmail.com>
2023-04-21 15:17:23 +0700

    fix api-client.js

--- src/api-client.js
+++ src/api-client.js
@@ -3,13 +3,11 @@ import axios from 'axios';
 import { uniqueId } from 'lodash';
 import parseData from './parser.js';
 
-const getRss = (url, watchedState, isUpdate = false) => axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
+const getRss = (url, watchedState) => axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
   .then((response) => {
     if (response.data.status.http_code === 200 && response.data.status.content_type.includes('xml')) {
-      if (!isUpdate) {
-        watchedState.urlSubmitProcess.errorKey = 'success';
-        watchedState.urlSubmitProcess.state = 'success';
-      }
+      watchedState.urlSubmitProcess.errorKey = 'success';
+      watchedState.urlSubmitProcess.state = 'success';
       return response.data.contents;
     }
     throw new Error('Invalid RSS');
