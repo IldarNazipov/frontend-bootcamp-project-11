bbe9d7bc606324a70d669fe1d6a42df836bf3037
Ildar Nazipov <ildar.nazipov@gmail.com>
2023-04-21 19:50:54 +0700

    fix

--- src/app.js
+++ src/app.js
@@ -7,23 +7,23 @@ import fetchRss from './api-client.js';
 export default () => {
   const rssFormEl = document.querySelector('.rss-form');
   const modal = document.getElementById('modal');
-  const watchedState = render(state);
+  const view = render(state);
 
   modal.addEventListener('show.bs.modal', (event) => {
     const button = event.relatedTarget;
     const targetId = button.getAttribute('data-id');
-    watchedState.uiState.modal.targetPostId = targetId;
-    watchedState.uiState.visitedIds.push(targetId);
+    view.uiState.modal.targetPostId = targetId;
+    view.uiState.visitedIds.push(targetId);
   });
 
   rssFormEl.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    watchedState.urlSubmitProcess.inputData.website = formData.get('url');
-    watchedState.urlSubmitProcess.state = 'sending';
-    validate(state.urlSubmitProcess.inputData, watchedState).then(() => {
+    view.urlSubmitProcess.inputData.website = formData.get('url');
+    view.urlSubmitProcess.state = 'sending';
+    validate(state.urlSubmitProcess.inputData, view).then(() => {
       if (state.urlSubmitProcess.state === 'valid') {
-        fetchRss(state.urlSubmitProcess.inputData.website, watchedState);
+        fetchRss(state.urlSubmitProcess.inputData.website, view);
       }
     });
   });
